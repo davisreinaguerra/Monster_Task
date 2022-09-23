53fe7933f4d21901344da41c569e526047d545bb
Davis Philip Reina-Guerra <105831652+davisreinaguerra@users.noreply.github.com>
2022-09-23 12:57:00 -0400

    Add files via upload

---
+++ MATLAB code/open_door.m
@@ -0,0 +1,5 @@
+% When called, this function will open the door of the foraging chamber
+
+function open_door
+    
+end
---
+++ MATLAB code/trigger_threat.m
@@ -0,0 +1,8 @@
+% When called, this function will write a pin HIGH to engage a threat
+
+function trigger_threat
+    % wait for IR beam to be broken
+    % when beam is broken
+    % Write an arduino pin to be HIGH, which will engage a While-High,
+    % do-this type deal (can this be done all in hardware?
+end
