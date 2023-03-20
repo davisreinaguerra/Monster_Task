e093b1966a3adc9b9c56fa0543ad49279ee07cd1
Davis Philip Reina-Guerra <105831652+davisreinaguerra@users.noreply.github.com>
2023-03-20 10:04:52 -0400

    Delete MATLAB directory

--- MATLAB/reset_stopwatches.m
+++
@@ -1,9 +0,0 @@
-% this code resets all stopwatches to zero
-
-function reset_stopwatches
-    % set door_open_stopwatch to zero
-    % set threat_engage_stopwatch to zero
-    % set deliver_reward_stopwatch to zero
-    % set door_close_threat_disengage_stopwatch to zero
-end
-
