a52066543b6bca25bf8aa3a77057252de16a6e3b
Davis Philip Reina-Guerra <105831652+davisreinaguerra@users.noreply.github.com>
2023-03-24 11:58:55 -0400

    Update sound_threat.ino

--- sound_threat.ino
+++ sound_threat.ino
@@ -8,7 +8,11 @@
 #define CARDCS 4                // Card chip select pin
 #define DREQ 3                  // VS1053 Data request, ideally an Interrupt pin
 
-#define alignmentPin 8
+
+#define trig_pin 13
+
+
+bool trig;
 
 
 
@@ -19,6 +23,8 @@ Adafruit_VS1053_FilePlayer musicPlayer = Adafruit_VS1053_FilePlayer(SHIELD_RESET
 void setup() {
   Serial.begin(9600);
 
+  pinMode(trig_pin, INPUT);
+  
   // Check for Connections
   if (!musicPlayer.begin()) {while (1);}
   Serial.println("Music Player found");
@@ -27,68 +33,21 @@ void setup() {
   
   // Set volume for (left, right) channels 
   musicPlayer.setVolume(40,40); // lower numbers == louder volume!
-
-  while (Serial.available() == 0) {}     //wait for data available
-
 }
 
 void loop() {
 
+  trig = digitalRead(trig_pin);
 
   if(trig == LOW) {
-    Serial.println("Waiting for trigger");
-    digitalWrite(move_pin, LOW);
-
-    // check for trigger
-    trig = digitalRead(trig_pin);
+    Serial.println("Waiting");
   } 
 
-  sound_and_alignment(alignmentPin);
-  
-}
-  
-void sound_and_alignment(int x) {
-  digitalWrite(x,HIGH);
-  musicPlayer.playFullFile("/GODZILLA.WAV");
-  digitalWrite(x,LOW);
-}
-  
-  
-  
-  
-  /* Code for the random sound generator
-
-  int randomN = random(0,100);
-  Serial.println(randomN);
-  bool play_sound = checkProb(sound_probability, randomN);
-  if (play_sound) {
-    sound_and_alignment(alignmentPin);
-  } 
-  else {
-    delay(sound_duration);
+  if(trig == HIGH) {
+    Serial.println("Play that funky music");
+    musicPlayer.playFullFile("/GODZILLA.WAV");
   }
 
-  */
-
-
-
-
-
-
-
-
-/* Code for the random sound generator
-
-bool checkProb(int x, int y) {
-  if (y < x) {
-    return true;
-  } else {return false;}
+  delay(50);
+  
 }
-
-void sound_and_alignment(int x) {
-  digitalWrite(x,HIGH);
-  musicPlayer.playFullFile("/PINK_N~1.WAV");
-  digitalWrite(x,LOW);
-}
-
-*/
