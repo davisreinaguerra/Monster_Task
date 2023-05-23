de6633c0a3b191f02811b42feed8859a9437ef81
Davis Reina-Guerra <105831652+davisreinaguerra@users.noreply.github.com>
2023-05-23 07:46:53 -0400

    Add files via upload

--- matlab_control/monsterapp.m
+++ matlab_control/monsterapp.m
@@ -2,22 +2,36 @@ classdef monsterapp < matlab.apps.AppBase
 
     % Properties that correspond to app components
     properties (Access = public)
-        UIFigure                      matlab.ui.Figure
-        StartButton                   matlab.ui.control.Button
-        SoundCheckBox                 matlab.ui.control.CheckBox
-        MonsterCheckBox               matlab.ui.control.CheckBox
-        FibPhotCheckBox               matlab.ui.control.CheckBox
-        msLabel_3                     matlab.ui.control.Label
-        msLabel_2                     matlab.ui.control.Label
-        msLabel                       matlab.ui.control.Label
-        RewardVolumeEditField         matlab.ui.control.NumericEditField
-        RewardVolumeEditFieldLabel    matlab.ui.control.Label
-        EnterTimeLimitEditField       matlab.ui.control.NumericEditField
-        EnterTimeLimitLabel           matlab.ui.control.Label
-        IntertrialIntervalEditField   matlab.ui.control.NumericEditField
-        IntertrialIntervalEditFieldLabel  matlab.ui.control.Label
-        NumberofTrialsEditField       matlab.ui.control.NumericEditField
-        NumberofTrialsEditFieldLabel  matlab.ui.control.Label
+        
+        % UI Properties
+        UIFigure                            matlab.ui.Figure
+        StartButton                         matlab.ui.control.Button
+        SoundCheckBox                       matlab.ui.control.CheckBox
+        MonsterCheckBox                     matlab.ui.control.CheckBox
+        FibPhotCheckBox                     matlab.ui.control.CheckBox
+        msLabel_3                           matlab.ui.control.Label
+        msLabel_2                           matlab.ui.control.Label
+        msLabel                             matlab.ui.control.Label
+        RewardVolumeEditField               matlab.ui.control.NumericEditField
+        RewardVolumeEditFieldLabel          matlab.ui.control.Label
+        EnterTimeLimitEditField             matlab.ui.control.NumericEditField
+        EnterTimeLimitLabel                 matlab.ui.control.Label
+        IntertrialIntervalEditField         matlab.ui.control.NumericEditField
+        IntertrialIntervalEditFieldLabel    matlab.ui.control.Label
+        NumberofTrialsEditField             matlab.ui.control.NumericEditField
+        NumberofTrialsEditFieldLabel        matlab.ui.control.Label
+        DateEditField                       matlab.ui.control.EditField
+        DateEditFieldLabel                  matlab.ui.control.Label
+        AnimalCodeEditField                 matlab.ui.control.EditField
+        AnimalCodeEditFieldLabel            matlab.ui.control.Label
+        DayDropDown                         matlab.ui.control.DropDown
+        DayDropDownLabel                    matlab.ui.control.Label
+        
+        
+        %Variables
+        date
+        animal_code
+        day
         arduino_Obj
         n_trials
         sound_qm
@@ -26,6 +40,7 @@ classdef monsterapp < matlab.apps.AppBase
         reward_volume
         intertrial_interval
         enter_time_limit
+        start_qm
     end
 
     % Callbacks that handle component events
@@ -40,9 +55,6 @@ classdef monsterapp < matlab.apps.AppBase
             writeline(app.arduino_Obj, num2str(app.monster_qm));
             writeline(app.arduino_Obj, num2str(app.sound_qm));
             writeline(app.arduino_Obj, "1");
-            if (app.fibphot_qm == 1)
-                disp("Run the Photometry record function!");
-            end
             delete(app.UIFigure);
             delete(app);
         end
@@ -88,6 +100,26 @@ classdef monsterapp < matlab.apps.AppBase
             value = app.NumberofTrialsEditField.Value;
             app.n_trials = value;
         end
+
+
+        %%%%%%%%%%%%%
+        % Value changed function: DateEditField
+        function date_enter(app, event)
+            value = app.DateEditField.Value;
+            app.date = value;
+        end
+
+        function animal_code_enter(app, event)
+            value = app.AnimalCodeEditField.Value;
+            app.animal_code = value;
+        end
+
+        function day_select(app, event)
+            value = app.DayDropDown.Value;
+            app.day = value;
+        end
+
+
     end
 
     % Component initialization
@@ -101,98 +133,152 @@ classdef monsterapp < matlab.apps.AppBase
 
             % Create UIFigure and hide until all components are created
             app.UIFigure = uifigure('Visible', 'off');
-            app.UIFigure.Position = [100 100 412 380];
+            app.UIFigure.Position = [0 0 412 470];
             app.UIFigure.Name = 'MATLAB App';
+
+
+            % Create DayDropDownLabel
+            app.DayDropDownLabel = uilabel(app.UIFigure);
+            app.DayDropDownLabel.HorizontalAlignment = 'right';
+            app.DayDropDownLabel.FontSize = 18;
+            app.DayDropDownLabel.Position = [25 430 142 23];
+            app.DayDropDownLabel.Text = 'Day';
+
+            % Create DayDropDown
+            app.DayDropDown = uidropdown(app.UIFigure);
+            app.DayDropDown.Items = {'Training1', 'Training2', 'Training3', 'NoMonster1', 'Monster2', 'NoMonster3', 'Monster4', 'NoMonster5', 'Monster6', 'NoMonster7'};
+            app.DayDropDown.ValueChangedFcn = createCallbackFcn(app, @day_select, true);
+            app.DayDropDown.Position = [186 430 200 22];
+            app.DayDropDown.Value = 'Training1';
+            
+
+            % Create AnimalCodeEditFieldLabel
+            app.AnimalCodeEditFieldLabel = uilabel(app.UIFigure);
+            app.AnimalCodeEditFieldLabel.HorizontalAlignment = 'right';
+            app.AnimalCodeEditFieldLabel.FontSize = 18;
+            app.AnimalCodeEditFieldLabel.Position = [25 400 142 23];
+            app.AnimalCodeEditFieldLabel.Text = 'AnimalCode:';
+
+            % Create AnimalCodeEditField
+            app.AnimalCodeEditField = uieditfield(app.UIFigure, 'text');
+            app.AnimalCodeEditField.ValueChangedFcn = createCallbackFcn(app, @animal_code_enter, true);
+            app.AnimalCodeEditField.Placeholder = 'Monster*_0*';
+            app.AnimalCodeEditField.Position = [186 400 200 22];
+
+            
+            % Create DateEditFieldLabel
+            app.DateEditFieldLabel = uilabel(app.UIFigure);
+            app.DateEditFieldLabel.HorizontalAlignment = 'right';
+            app.DateEditFieldLabel.FontSize = 18;
+            app.DateEditFieldLabel.Position = [25 370 142 23];
+            app.DateEditFieldLabel.Text = 'Date:';
+
+            % Create DateEditField
+            app.DateEditField = uieditfield(app.UIFigure, 'text');
+            app.DateEditField.ValueChangedFcn = createCallbackFcn(app, @date_enter, true);
+            app.DateEditField.Placeholder = 'Day_Month_Year';
+            app.DateEditField.Position = [186 370 200 22];
+
+            %______________________Numeric Edit Fields ___________________
 
             % Create NumberofTrialsEditFieldLabel
             app.NumberofTrialsEditFieldLabel = uilabel(app.UIFigure);
             app.NumberofTrialsEditFieldLabel.HorizontalAlignment = 'right';
             app.NumberofTrialsEditFieldLabel.FontSize = 18;
-            app.NumberofTrialsEditFieldLabel.Position = [25 343 142 23];
+            app.NumberofTrialsEditFieldLabel.Position = [25 340 142 23];
             app.NumberofTrialsEditFieldLabel.Text = 'Number of Trials:';
 
             % Create NumberofTrialsEditField
             app.NumberofTrialsEditField = uieditfield(app.UIFigure, 'numeric');
             app.NumberofTrialsEditField.ValueChangedFcn = createCallbackFcn(app, @n_trials_enter, true);
-            app.NumberofTrialsEditField.Position = [186 343 58 22];
+            app.NumberofTrialsEditField.Position = [186 340 58 22];
+
 
             % Create IntertrialIntervalEditFieldLabel
             app.IntertrialIntervalEditFieldLabel = uilabel(app.UIFigure);
             app.IntertrialIntervalEditFieldLabel.HorizontalAlignment = 'right';
             app.IntertrialIntervalEditFieldLabel.FontSize = 18;
-            app.IntertrialIntervalEditFieldLabel.Position = [28 312 139 23];
+            app.IntertrialIntervalEditFieldLabel.Position = [28 310 139 23];
             app.IntertrialIntervalEditFieldLabel.Text = 'Intertrial Interval:';
 
             % Create IntertrialIntervalEditField
             app.IntertrialIntervalEditField = uieditfield(app.UIFigure, 'numeric');
             app.IntertrialIntervalEditField.ValueChangedFcn = createCallbackFcn(app, @intertrial_interval_enter, true);
-            app.IntertrialIntervalEditField.Position = [186 312 58 22];
+            app.IntertrialIntervalEditField.Position = [186 310 58 22];
+
 
             % Create EnterTimeLimitLabel
             app.EnterTimeLimitLabel = uilabel(app.UIFigure);
             app.EnterTimeLimitLabel.HorizontalAlignment = 'right';
             app.EnterTimeLimitLabel.FontSize = 18;
-            app.EnterTimeLimitLabel.Position = [27 279 140 23];
+            app.EnterTimeLimitLabel.Position = [27 280 140 23];
             app.EnterTimeLimitLabel.Text = 'Enter Time Limit:';
 
             % Create EnterTimeLimitEditField
             app.EnterTimeLimitEditField = uieditfield(app.UIFigure, 'numeric');
             app.EnterTimeLimitEditField.ValueChangedFcn = createCallbackFcn(app, @enter_time_limit_enter, true);
-            app.EnterTimeLimitEditField.Position = [186 279 58 22];
+            app.EnterTimeLimitEditField.Position = [186 280 58 22];
+
 
             % Create RewardVolumeEditFieldLabel
             app.RewardVolumeEditFieldLabel = uilabel(app.UIFigure);
             app.RewardVolumeEditFieldLabel.HorizontalAlignment = 'right';
             app.RewardVolumeEditFieldLabel.FontSize = 18;
-            app.RewardVolumeEditFieldLabel.Position = [30 244 137 23];
+            app.RewardVolumeEditFieldLabel.Position = [30 250 137 23];
             app.RewardVolumeEditFieldLabel.Text = 'Reward Volume:';
 
             % Create RewardVolumeEditField
             app.RewardVolumeEditField = uieditfield(app.UIFigure, 'numeric');
             app.RewardVolumeEditField.ValueChangedFcn = createCallbackFcn(app, @reward_volume_enter, true);
-            app.RewardVolumeEditField.Position = [186 244 58 22];
+            app.RewardVolumeEditField.Position = [186 250 58 22];
+
+            %____________msLabels________________________________
 
             % Create msLabel
             app.msLabel = uilabel(app.UIFigure);
-            app.msLabel.HorizontalAlignment = 'right';
+            app.msLabel.HorizontalAlignment = 'left';
             app.msLabel.FontSize = 18;
-            app.msLabel.Position = [243 312 29 23];
+            app.msLabel.Position = [250 310 29 23];
             app.msLabel.Text = 's';
 
             % Create msLabel_2
             app.msLabel_2 = uilabel(app.UIFigure);
-            app.msLabel_2.HorizontalAlignment = 'right';
+            app.msLabel_2.HorizontalAlignment = 'left';
             app.msLabel_2.FontSize = 18;
-            app.msLabel_2.Position = [243 279 29 23];
+            app.msLabel_2.Position = [250 280 29 23];
             app.msLabel_2.Text = 's';
 
             % Create msLabel_3
             app.msLabel_3 = uilabel(app.UIFigure);
-            app.msLabel_3.HorizontalAlignment = 'right';
+            app.msLabel_3.HorizontalAlignment = 'left';
             app.msLabel_3.FontSize = 18;
-            app.msLabel_3.Position = [243 244 29 23];
+            app.msLabel_3.Position = [250 250 29 23];
             app.msLabel_3.Text = 'ms';
+
+            %______________Checkboxes______________________________________
 
             % Create MonsterCheckBox
             app.MonsterCheckBox = uicheckbox(app.UIFigure);
             app.MonsterCheckBox.ValueChangedFcn = createCallbackFcn(app, @monster_qm_check, true);
             app.MonsterCheckBox.Text = 'Monster';
             app.MonsterCheckBox.FontSize = 18;
-            app.MonsterCheckBox.Position = [300 320 87 22];
+            app.MonsterCheckBox.Position = [300 340 87 22];
 
             % Create SoundCheckBox
             app.SoundCheckBox = uicheckbox(app.UIFigure);
             app.SoundCheckBox.ValueChangedFcn = createCallbackFcn(app, @sound_qm_check, true);
             app.SoundCheckBox.Text = 'Sound';
             app.SoundCheckBox.FontSize = 18;
-            app.SoundCheckBox.Position = [300 280 87 22];
+            app.SoundCheckBox.Position = [300 300 87 22];
 
             % Create FibphotCheckBox
             app.FibPhotCheckBox = uicheckbox(app.UIFigure);
             app.FibPhotCheckBox.ValueChangedFcn = createCallbackFcn(app, @fibphot_qm_check, true);
             app.FibPhotCheckBox.Text = 'FibPhot';
             app.FibPhotCheckBox.FontSize = 18;
-            app.FibPhotCheckBox.Position = [300 240 87 22];
+            app.FibPhotCheckBox.Position = [300 260 87 22];
+
+            %_____________Start Button_____________________________________
 
             % Create StartButton
             app.StartButton = uibutton(app.UIFigure, 'push');
@@ -228,6 +314,9 @@ classdef monsterapp < matlab.apps.AppBase
 
             app.sound_qm = 0;
             app.monster_qm = 0;
+            app.fibphot_qm = 0;
+            app.start_qm = 0;
+            
 
             if nargout == 0
                 clear app
