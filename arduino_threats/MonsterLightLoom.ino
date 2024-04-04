66f4aae3648696142ca1695021bb69ca25f4eca0
Davis Reina-Guerra <105831652+davisreinaguerra@users.noreply.github.com>
2024-04-04 16:26:26 -0400

    Add files via upload

---
+++ arduino_threats/MonsterLightLoom.ino
@@ -0,0 +1,52 @@
+// Adafruit protomatter definition
+#include <Adafruit_Protomatter.h>
+// Color definitions
+#define BLACK    0x0000
+#define BLUE     0x001F
+#define RED      0xF800
+#define GREEN    0x07E0
+#define CYAN     0x07FF
+#define MAGENTA  0xF81F
+#define YELLOW   0xFFE0 
+#define WHITE    0xFFFF
+
+uint8_t rgbPins[]  = {7, 8, 9, 10, 11, 12};
+uint8_t addrPins[] = {17, 18, 19, 20, 21};
+uint8_t clockPin   = 14;
+uint8_t latchPin   = 15;
+uint8_t oePin      = 16;
+
+// adafruit LED matrix object
+Adafruit_Protomatter matrix(
+  64,          // Width of matrix (or matrix chain) in pixels
+  4,           // Bit depth, 1-6
+  1, rgbPins,  // # of matrix chains, array of 6 RGB pins for each
+  5, addrPins, // # of address pins (height is inferred), array of pins
+  clockPin, latchPin, oePin, // Other matrix control pins
+  false);      // No double-buffering here (see "doublebuffer" example)
+
+void setup(void) {
+  Serial.begin(9600);
+
+  // Initialize matrix...
+  ProtomatterStatus status = matrix.begin();
+  Serial.print("Protomatter begin() status: ");
+  Serial.println((int)status);
+  if(status != PROTOMATTER_OK) {
+    for(;;);
+  }
+}
+
+void loop(void) {
+
+  matrix.fillScreen(WHITE);
+  matrix.show();
+  delay(1000);
+
+  for (int i = 0; i <= 50; i++) {
+    //matrix.fillScreen(WHITE);
+    matrix.fillCircle(32, 32, i, BLACK);
+    matrix.show();
+    delay(i/2);
+  }
+}
